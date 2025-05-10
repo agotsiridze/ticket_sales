82f3054e63036758c07f75d699796708f8eb711d
agotsiridze <a.gotsiridze@ssu.edu.ge>
2025-05-10 17:43:03 +0400

    migrations env file fix

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -10,25 +10,16 @@ from utils import DATABASE_URL, engine
 
 target_metadata = Base.metadata
 
-config = context.config
-config.set_main_option("sqlalchemy.url", str(DATABASE_URL))
+def create_offline_config():
+    config = context.config
+    config.set_main_option("sqlalchemy.url", str(DATABASE_URL))
 
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
+    if config.config_file_name is not None:
+        fileConfig(config.config_file_name)
+    return config
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
+    config = create_offline_config()
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -48,15 +39,11 @@ def do_run_migrations(connection: Connection) -> None:
         context.run_migrations()
 
 
-
 async def run_async_migrations() -> None:
     
     async with engine.begin() as conn:
         await conn.run_sync(do_run_migrations)
     await engine.dispose()
-
-
-
 
 
 def run_migrations_online() -> None:
