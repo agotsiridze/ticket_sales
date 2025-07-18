92c723035c5b7dc66840d7577334ca560281cd1c
agotsiridze <a.gotsiridze@ssu.edu.ge>
2025-07-18 17:42:15 +0400

    password hashing fix

--- backend/services/password_hash.py
+++ backend/services/password_hash.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from passlib.context import CryptContext
+import bcrypt
 
 
 class PasswordEncode(ABC):
@@ -14,11 +14,14 @@ class PasswordEncode(ABC):
 
 
 class BCryptPasswordEncode(PasswordEncode):
-    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    # pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    encoding = 'utf-8'
     
     def hash_password(self, password: str) -> str:
-        return self.pwd_context.hash(password)
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode(self.encoding), salt)
+        return hashed.decode(self.encoding)
     
     
     def verify_password(self, plain_password: str, hashed_password: str) -> bool:
-        return self.pwd_context.verify(plain_password, hashed_password)
+        return bcrypt.checkpw(plain_password.encode(self.encoding), hashed_password.encode(self.encoding))
